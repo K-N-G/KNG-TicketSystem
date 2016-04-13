50a409a60a8a989bc02a2288df1c0b3af9139e8f
goodprice <rrr_ood@yahoo.com>
2016-04-13 18:15:33 +0300

    Project
    Project ot HTML to AngularJS

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -8,7 +8,24 @@ angular.module("KNGSoftware.projects",['ngRoute'])
         });
     }])
     .controller('projectsController',['$scope',function($scope){
-        $scope.projects = {
-            "name": "projects"
-        };
+        $scope.projects = [
+            {
+                "h3": "Online магазин за машини и инструменти",
+                "href": "http://goodprice.bg",
+                "src": ".../../picture/goodprice.png",
+                "alt": "Goodprice logo"
+            },
+            {
+                "h3": "Online магазин за красота",
+                "href": "http://goodface.eu",
+                "src": ".../../picture/goodface.png",
+                "alt": "Goodface logo"
+            },
+            {
+                "h3": "Българския форум",
+                "href": "http://forum-bulgaria.eu",
+                "src": ".../../picture/forum-bulgaria.png",
+                "alt": "Forum Bulgaria logo"
+            }
+        ];
     }]);
